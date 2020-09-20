d307cf2f94e4b6288b1cd2ddbb3ed18c7ec1e021
Christian Smith <cgr.gonzalezrossier@gmail.com>
2020-09-20 12:33:30 -0300

    Refactoring

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { UserModule } from './user/user.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '*' });
+  const app = await NestFactory.create(UserModule);
+  app.enableCors({ origin: 'http://localhost:4200' });
   await app.listen(3000);
 }
 bootstrap();
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,19 @@
-import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { User, UserSchema } from './schemas/user.schema';
 
+// Mongodb
+import { MongooseModule } from '@nestjs/mongoose';
+
+// Env var
+import { config } from 'dotenv';
+
+config();
+
 @Module({
   imports: [
+    MongooseModule.forRoot(process.env.DB_URI),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [UserController],
