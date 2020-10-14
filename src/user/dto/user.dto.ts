a9277cba9dd64969e7faf1fde12cadf77f4d9c3d
Christian Smith <cgr.gonzalezrossier@gmail.com>
2020-10-14 11:02:59 -0300

    Se agregaron ejemplos a la seccion /docs

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,11 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, IsString, MinLength } from 'class-validator';
+import { IsNumber, IsString, Max, Min, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty({
     description: 'Rut identificador del usuario.',
     type: Number,
     required: true,
+    example: 12345678,
+  })
+  @Max(99999999, { message: 'El campo <rut> debe ser de máximo 8 caracteres.' })
+  @Min(1000000, {
+    message: 'El campo <rut> debe ser de al menos 7 caracteres.',
   })
   @IsNumber({}, { message: 'El campo <rut> debe ser número.' })
   readonly rut: number;
@@ -15,6 +20,7 @@ export class CreateUserDto {
     minLength: 3,
     type: String,
     required: true,
+    example: 'Juan',
   })
   @MinLength(3, {
     message: 'El campo <name> debe contener al menos 3 caracteres.',
@@ -27,6 +33,7 @@ export class CreateUserDto {
     minLength: 3,
     type: String,
     required: true,
+    example: 'Perez',
   })
   @MinLength(3, {
     message: 'El campo <lastname> debe contener al menos 3 caracteres.',
@@ -38,6 +45,13 @@ export class CreateUserDto {
     description: 'Número del usuario.',
     type: Number,
     required: true,
+    example: 87654321,
+  })
+  @Max(99999999, {
+    message: 'El campo <phonenumber> debe ser de máximo 8 caracteres.',
+  })
+  @Min(10000000, {
+    message: 'El campo <phonenumber> debe ser de al menos 8 caracteres.',
   })
   @IsNumber({}, { message: 'El campo <phonenumber> debe ser número.' })
   readonly phonenumber: number;
